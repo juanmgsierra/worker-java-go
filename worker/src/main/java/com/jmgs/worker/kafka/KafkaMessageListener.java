41c804ac380ff54a14f0d75cadc5d48df068d82f
Juan Miguel Garcia <47489189+juanmgsierra@users.noreply.github.com>
2026-03-10 00:53:33 -0600

    add: Kafka listener Redis/Kafka config

---
+++ worker/src/main/java/com/jmgs/worker/kafka/KafkaMessageListener.java
@@ -0,0 +1,21 @@
+package com.jmgs.worker.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.jmgs.worker.dto.OrderEvent;
+import com.jmgs.worker.service.ServiceWorker;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class KafkaMessageListener {
+
+    private final ServiceWorker serviceWorker;
+
+    @KafkaListener(topics = "order")
+    public void listen(OrderEvent order) {
+        serviceWorker.saveOrder(order);
+    }
+}
